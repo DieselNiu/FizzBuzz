c0a455f46f8531fa43aae2c605b7914e93e0954c
dieselniu <niu853273494@qq.com>
2021-06-17 16:23:48 +0800

    complete GameNumber

--- src/main/java/GameNumber.java
+++ src/main/java/GameNumber.java
@@ -1,4 +1,27 @@
+import static java.lang.String.*;
+
 public class GameNumber {
+    private int rawNumber;
+
     public GameNumber(int rawNumber) {
+        this.rawNumber = rawNumber;
+    }
+
+    @Override
+    public String toString() {
+        if (isRelatedTo(15)) {
+            return "FizzBuzz";
+        }
+        if (isRelatedTo(3)) {
+            return "Fizz";
+        }
+        if (isRelatedTo(5)) {
+            return "Buzz";
+        }
+        return valueOf(rawNumber);
+    }
+
+    private boolean isRelatedTo(int i) {
+        return rawNumber % i == 0|| valueOf(rawNumber).contains(valueOf(i));
     }
 }
--- src/test/java/GameNumberTest.java
+++ src/test/java/GameNumberTest.java
@@ -2,10 +2,21 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-
 public class GameNumberTest {
     @Test
-    public void should_(){
-        assertThat(new GameNumber(1).toString).isEqualTo("1");
+    public void should_say_1_when_raw_number_is_1(){
+      assertThat(new GameNumber(1).toString()).isEqualTo("1");
+    }
+    @Test
+    public void should_say_fizz_when_raw_number_is_3(){
+      assertThat(new GameNumber(3).toString()).isEqualTo("Fizz");
+    }
+    @Test
+    public void should_say_buzz_when_raw_number_is_5(){
+        assertThat(new GameNumber(5).toString()).isEqualTo("Buzz");
+    }
+    @Test
+    public void should_say_fizzbuzz_when_raw_number_is_15(){
+      assertThat(new GameNumber(15).toString()).isEqualTo("FizzBuzz");
     }
 }

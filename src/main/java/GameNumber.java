7f026d97dda08b90bd62f8d8f5d8ea85545cd69e
dieselniu <niu853273494@qq.com>
2021-09-12 10:14:03 +0800

    UPDATED: GameNumber.toString

--- src/main/java/GameNumber.java
+++ src/main/java/GameNumber.java
@@ -10,6 +10,9 @@ public class GameNumber {
         if (rawNumber % 3 == 0) {
             return "Fizz";
         }
+        if (rawNumber % 5 == 0) {
+            return "Buzz";
+        }
         return String.valueOf(rawNumber);
     }
 }
--- src/test/java/GameNumberTest.java
+++ src/test/java/GameNumberTest.java
@@ -4,16 +4,22 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class GameNumberTest {
     @Test
-    public void should_say_1_when_game_number_is_1(){
+    public void should_say_1_when_game_number_is_1() {
         GameNumber gameNumber = new GameNumber(1);
         assertThat(gameNumber.toString()).isEqualTo("1");
     }
 
-  @Test
-  public void should_say_fizz_when_game_number_is_3(){
-      GameNumber gameNumber = new GameNumber(3);
-      assertThat(gameNumber.toString()).isEqualTo("Fizz");
+    @Test
+    public void should_say_fizz_when_game_number_is_3() {
+        GameNumber gameNumber = new GameNumber(3);
+        assertThat(gameNumber.toString()).isEqualTo("Fizz");
 
-  }
+    }
+
+    @Test
+    public void should_say_buzz_when_raw_number_is_5() {
+        assertThat(new GameNumber(5).toString()).isEqualTo("Buzz");
+
+    }
 }
 
